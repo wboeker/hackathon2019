95a4b1b788ed38c2e775ac6f4a7ea5e0ea90e75f
dyao0210 <dyao0210@users.noreply.github.com>
2019-02-02 16:53:58 -0500

    Update testin.py

--- testin.py
+++ testin.py
@@ -4,27 +4,26 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
-store = file.Storage('token.pickle')#File that stores user login data -- make fiel beforehand can just be empty i think
+store = file.Storage('token.pickle')#File that stores user login data -- make file beforehand can just be empty i think
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('credentials.json', SCOPES)# file from google
     creds = tools.run_flow(flow, store)
 GCAL = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
-GMT_OFF = '-07:00'      # PDT/MST/GMT-7
-EVENT = {
-    'summary': 'The Real Test',
-    'start':  {'dateTime': '2019-02-13T19:00:00%s' % GMT_OFF},
-    'end':    {'dateTime': '2019-02-13T22:00:00%s' % GMT_OFF},
-    'location': 'Georgetown University',
-    'description': 'This is a very nice test event please come we are desperate!',
-    'attendees': [
-        {'email': 'test1@email.com'},
-        {'email': 'test2@email.com'},
-    ],
-}
+GMT_OFF = '-05:00'      # Now in EST
 
-e = GCAL.events().insert(calendarId='3vh66p2d72g0gnv7cl5m06s42o@group.calendar.google.com',
+#currently looping from 0-5 (6 times)
+for x in range(0, 6):
+    EVENT = {
+        'summary': 'The Real Test',
+        'start':  {'dateTime': '2019-02-13T19:00:00%s' % GMT_OFF},
+        'end':    {'dateTime': '2019-02-13T22:00:00%s' % GMT_OFF},
+        'location': 'Georgetown University',
+        'description': 'This is a very nice test event please come we are desperate!',
+    }
+
+    e = GCAL.events().insert(calendarId='3vh66p2d72g0gnv7cl5m06s42o@group.calendar.google.com',
         sendNotifications=True, body=EVENT).execute()
 
 print('''*** %r event added:
